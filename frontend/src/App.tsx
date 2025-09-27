d69b6c531ca8e5d5d17d2c27a2974f8108e70269
Qvadoch <ogalevd@gmail.com>
2025-09-27 16:23:56 +0300

    сделал

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1>Smart Task Manager</h1>
+            <p>Ваше приложение будет здесь</p>
+        </div>
+    );
+};
+
+export default App;
